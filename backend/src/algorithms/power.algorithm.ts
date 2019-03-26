8c29cb56a7d1bcad3f496ec83d91444cea9b36f4
Thomas <thomas.mandemaker@hotmail.com>
2019-03-26 22:23:11 +0100

    [Added] Power thing

--- backend/src/algorithms/power.algorithm.ts
+++ backend/src/algorithms/power.algorithm.ts
@@ -1,2 +1,23 @@
-const cost = 0.2292;
+import { getRepository } from 'typeorm';
+import { Device, DeviceType } from '../entities';
 
+export class PowerAlgorithm {
+  private readonly cost = 0.0002292;
+  private totalWatt = 0;
+  private totalCost = 0;
+
+    async calc () {
+      const res = getRepository(Device);
+      let tempWatt = 0;
+      const objs = await res.find();
+
+      for(const item of objs) {
+        if (item.status){
+          tempWatt++;
+        }
+      }
+
+      this.totalCost += this.cost * 1/60 * tempWatt;
+      this.totalWatt += tempWatt;
+    }
+};
